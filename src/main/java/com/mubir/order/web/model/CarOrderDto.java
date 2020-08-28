db9a01ff5dcc648396093995c7a50b7f5aa3372c
Munir <info.mubir@gmail.com>
2020-08-28 15:31:51 +0900

    base item class extends removed

--- src/main/java/com/mubir/order/web/model/CarOrderDto.java
+++ src/main/java/com/mubir/order/web/model/CarOrderDto.java
@@ -1,30 +1,36 @@
 package com.mubir.order.web.model;
 
-import lombok.Builder;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
 
 import java.time.OffsetDateTime;
 import java.util.List;
 import java.util.UUID;
+
 @Data
 @NoArgsConstructor
-@EqualsAndHashCode(callSuper = true)
-public class CarOrderDto extends BaseItem{
-    @Builder
-    public CarOrderDto(UUID id, Integer version, OffsetDateTime createdDate, OffsetDateTime lastModifiedDate, UUID customerId, String customerRef, List<CarOrderLineDto> carOrderLines, String orderStatus, String orderStatusCallbackUrl) {
-        super(id, version, createdDate, lastModifiedDate);
-        this.customerId = customerId;
-        this.customerRef = customerRef;
-        this.carOrderLines = carOrderLines;
-        this.orderStatus = orderStatus;
-        this.orderStatusCallbackUrl = orderStatusCallbackUrl;
-    }
+@Builder
+@AllArgsConstructor
+public class CarOrderDto {
+    @JsonProperty("id")
+    private UUID id = null; // order id not car id
+
+    @JsonProperty("version")
+    private Integer version = null;
+
+    @JsonFormat(pattern="yyyy-MM-dd'T'HH:mm:ssZ", shape=JsonFormat.Shape.STRING)
+    @JsonProperty("createdDate")
+    private OffsetDateTime createdDate = null;
+
+    @JsonFormat(pattern="yyyy-MM-dd'T'HH:mm:ssZ", shape=JsonFormat.Shape.STRING)
+    @JsonProperty("lastModifiedDate")
+    private OffsetDateTime lastModifiedDate = null;
 
     private UUID customerId;
     private String customerRef;
     private List<CarOrderLineDto> carOrderLines;
     private String orderStatus;
     private String orderStatusCallbackUrl;
+
 }
--- src/main/java/com/mubir/order/web/model/CarOrderLineDto.java
+++ src/main/java/com/mubir/order/web/model/CarOrderLineDto.java
@@ -1,35 +1,36 @@
 package com.mubir.order.web.model;
 
-import lombok.Builder;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
 
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.UUID;
 @Data
 @NoArgsConstructor
-@EqualsAndHashCode(callSuper = true)
-public class CarOrderLineDto extends BaseItem{
-    @Builder
-    public CarOrderLineDto(UUID id, Integer version, OffsetDateTime createdDate,
-                           OffsetDateTime lastModifiedDate, String upc,
-                           String carName, UUID carId, Integer orderQuantity,
-                           String carModel,BigDecimal price) {
-        super(id, version, createdDate, lastModifiedDate);
-        this.upc = upc;
-        this.carName = carName;
-        this.carId = carId;
-        this.orderQuantity = orderQuantity;
-        this.carModel = carModel;
-        this.price =price;
-    }
+@AllArgsConstructor
+@Builder
+public class CarOrderLineDto {
 
+    @JsonProperty("id")
+    private UUID id = null; // order id not car id
+
+    @JsonProperty("version")
+    private Integer version = null;
+
+    @JsonFormat(pattern="yyyy-MM-dd'T'HH:mm:ssZ", shape=JsonFormat.Shape.STRING)
+    @JsonProperty("createdDate")
+    private OffsetDateTime createdDate = null;
+
+    @JsonFormat(pattern="yyyy-MM-dd'T'HH:mm:ssZ", shape=JsonFormat.Shape.STRING)
+    @JsonProperty("lastModifiedDate")
+    private OffsetDateTime lastModifiedDate = null;
     private String upc;
     private String carName;
     private String carModel;
     private UUID carId;
     private BigDecimal price;
     private Integer orderQuantity = 0;
+    private Integer quantityAllocated;
 }
